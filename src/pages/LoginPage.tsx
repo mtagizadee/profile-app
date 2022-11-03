d5c5be38d35c7db95ee38548a63b5aa31e395490
mtagizadee <magatagi0@gmail.com>
2022-11-03 11:47:07 +0400

    init

---
+++ src/pages/LoginPage.tsx
@@ -0,0 +1,9 @@
+const LoginPage = () => {
+    return (
+        <div className="flex items-center justify-center h-screen font-bold text-xl">
+            Login Page
+        </div>
+    );
+}
+
+export default LoginPage;
